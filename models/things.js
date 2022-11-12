40f17da3cf68b2aa1053081f9f6330ad8a2c6e07
Julien.guzell <irfanguzel1997@gmail.com>
2022-11-12 23:05:45 +0100

    30%

---
+++ models/things.js
@@ -0,0 +1,19 @@
+// structure d'une sauce
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String },
+    heat: { type: Number, required: true },
+    likes: { type: Number, require: true, default: 0 },
+    dislikes: { type: Number, require: true, default: 0 },
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String] }
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
